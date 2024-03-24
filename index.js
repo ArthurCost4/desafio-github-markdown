7585663d88f506657f9171e6b2089fef69972cf5
ArthurCost4 <arthurlinhares69@gmail.com>
2024-03-24 18:25:35 -0300

    ainda com preguiça de saber pq esse karaio n renderiza o componente

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+const myComment = <h1>AOPA</h1>
+
 import App from "./src/App.js";
-
-/**?
- * 
- 
-const appRouting = (
-  <Router>
-    <Routes>
-      <Route exact path="/" component={App} />
-    </Routes>
-  </Router>
-);
-
-ReactDOM.render(appRouting, document.getElementById("root"));
-*/
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
-
 root.render(
   <StrictMode>
     <App />
